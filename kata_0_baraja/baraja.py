da50e747bc587384c8c4467934de228a284fa189
irene <irenebrave120@gmail.com>
2023-05-17 21:28:20 +0200

    version2

--- kata_0_baraja/baraja.py
+++ kata_0_baraja/baraja.py
@@ -1,3 +1,5 @@
+import random
+
 def crear_baraja(n,p):
     numeros= n
     palos=p
@@ -7,4 +9,10 @@ def crear_baraja(n,p):
         for numero in numeros:
             naipe = numero + palo
             baraja.append(naipe)
+    return baraja
+
+def barajar_for(baraja):
+    for i in range (len(baraja)):
+        j=random.randrange(len(baraja))
+        baraja[i],baraja[j] = baraja[j],baraja[i]
     return baraja
--- kata_0_baraja/main.py
+++ kata_0_baraja/main.py
@@ -1,2 +1,12 @@
 import baraja
 print(baraja.crear_baraja(['A','1','2','3','4','5','6','7','8','C','R'],['O','C','B','E']))
+
+
+b = baraja.crear_baraja("A23456789SCR","OCEB")
+print(b)
+print()
+print()
+print()
+
+baraja.barajar_for(b)
+print(b)
